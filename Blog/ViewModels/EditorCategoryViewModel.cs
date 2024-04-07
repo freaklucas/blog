7a3440ab1c8fcf0bf9235f678f9a58628a468dc9
freaklucas <lucas.oliveira.freak@gmail.com>
2024-04-07 18:43:58 -0300

    feat:validações de entrada.

--- Blog/ViewModels/EditorCategoryViewModel.cs
+++ Blog/ViewModels/EditorCategoryViewModel.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Blog.ViewModels;
 
 public class EditorCategoryViewModel
 {
+    [Required(ErrorMessage = "O nome da categoria é obrigatório.")]
+    [StringLength(40, MinimumLength = 3, ErrorMessage = "O nome deve conter entre 3 e 40 caracteres.")]
     public string? Name { get; set; }
+    [Required(ErrorMessage = "O slug da categoria é obrigatório.")]
+    [StringLength(40, MinimumLength = 3, ErrorMessage = "O slug deve conter entre 3 e 40 caracteres.")]
     public string? Slug { get; set; }
 }
