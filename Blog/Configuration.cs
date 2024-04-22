493c545c1688ab6afd0f02071cee277294455f56
freaklucas <lucas.oliveira.freak@gmail.com>
2024-04-21 21:43:06 -0300

    bug:consertando token de usuário

--- Blog/Configuration.cs
+++ Blog/Configuration.cs
@@ -2,5 +2,9 @@ namespace Blog;
 
 public static class Configuration
 {
-    public static string JWTKey { get; set; } = "WFd0ZklWbVhnMDYvOVpjaU84OFl0Zz09";
+    public static string? JWTKey = "WFd0ZklWbVhnMDYvOVpjaU84OFl0Zz09";
+
+    public static string? ApiKeyName = "api_key";
+
+    public static string? ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
 }
